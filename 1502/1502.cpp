55d589a6c71502af184b156d9d3aefec5602cb2e
UtaWashu <86579882+UtaWashu@users.noreply.github.com>
2024-05-30 09:45:58 +0300

    Create 1502.cpp

---
+++ 1502/1502.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+      std::sort(arr.begin(), arr.end());
+
+        int diff = arr[1] - arr[0]; // Calculate the common difference
+
+        for (int i = 1; i < arr.size() - 1; ++i) {
+            if (arr[i + 1] - arr[i] != diff) {
+                return false;
+            }
+        }
+
+        return true;  
+    }
+};
